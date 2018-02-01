b297bc5861d829506030beed8341b1202bc0ef74
Bert Overduin <boverduin@gmail.com>
2018-02-01 09:44:40 +0000

    Add correction exercise C.1.

--- homework.rb
+++ homework.rb
@@ -193,7 +193,7 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from "Swansea" to "Cardiff".
 
-united_kingdom[2][:name] = "Cardiff"
+united_kingdom[1][:capital] = "Cardiff"
 
 # 2. Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
 
